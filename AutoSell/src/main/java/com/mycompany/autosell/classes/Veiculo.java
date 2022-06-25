a92bf1c9c2ca2713af39991590322d8c583ccd72
Eduardo200156 <51315973+Eduardo200156@users.noreply.github.com>
2022-06-25 23:49:36 +0100

    Add files via upload

---
+++ AutoSell/src/main/java/com/mycompany/autosell/classes/Veiculo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.autosell.classes;
+
+import com.mycompany.autosell.modelo.veiculo.veiculo;
+
+
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Veiculo {
+
+    public static void main(String[] args) {
+        veiculo frm = new veiculo();
+        frm.setVisible(true);
+    }
+}
